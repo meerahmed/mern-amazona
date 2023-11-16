5beb310f91de54639195d3b5792ee208165ee0bb
MEER AHMED <meercse45@gmail.com>
2023-11-16 14:01:43 +0600

    Render Product List

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 2.  Install Tools
 3.  Create React App
 4.  Create Git Repository
+5.  List Products
+    1.Create Product Array 2. Add Product Image 3. Render Products 4. Style Products
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Amazona</a>
       </header>
-      <main>Product Lists</main>
+      <main>
+        <h1>Featured Product</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
